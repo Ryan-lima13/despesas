c53c50763d7577d18e27881c1b28857bbd82967d
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2022-04-20 18:15:29 -0300

    aplicando estilo nos textos

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:despesas/model/Transaction.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(Home());
+  
 }
 
 class Home extends StatelessWidget {
@@ -9,8 +11,106 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      home: Despesas(),
+    );
+  }
+}
+class Despesas extends StatelessWidget {
+  
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tenis de corrida', 
+      value: 310.0,
+       date: DateTime.now()
+      ),
+      Transaction(
+      id: 't2',
+      title: 'Conta luz', 
+      value: 211.30,
+       date: DateTime.now()
+      ),
+
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Despesas Pessoais"),
+        
+        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+       
+        children: [
+          Container(
+          
+            child: Card(
+              color: Colors.blue,
+              child: Text("grafico"),
+              elevation: 5,
+
+            ),
+          ),
+          Column(
+            children: _transactions.map((tr){
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10
+                      ),
+                      child: Text(
+                        tr.value.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+
+                        ),
+                        ),
+                        decoration: BoxDecoration(
+                          border:  Border.all(
+                            color: Colors.purple,
+                            width: 2,
+
+                            )
+                          ),
+                          padding: EdgeInsets.all(10),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16
+                          ),
+                          ),
+                        Text(
+                          tr.date.toString(),
+                          style: TextStyle(
+                            color: Colors.grey
+                          ),
+                          )
+                      ],
+                    )
+                  ]
+                  ),
+              );
+
+            }).toList(),
+           
+          )
+        ],
+      )
     );
   }
 }
