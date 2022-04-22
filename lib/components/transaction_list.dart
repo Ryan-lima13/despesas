76c0c7ab2e6f345aaebd69bb7408ce8df335775e
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2022-04-22 13:35:08 -0300

    atalizando listview

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -32,52 +32,31 @@ class TransactionList extends StatelessWidget {
         itemBuilder: (contx,index){
           final tr = transactions[index];
           return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10
-                        ),
-                        child: Text(
-                         'R\$ ${tr.value.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple,
-
-                          ),
-                          ),
-                          decoration: BoxDecoration(
-                            border:  Border.all(
-                              color: Colors.purple,
-                              width: 2,
-
-                              )
-                            ),
-                            padding: EdgeInsets.all(10),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                             tr.title,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16
-                            ),
-                            ),
-                          Text(
-                            DateFormat(' d MMM y').format(tr.date),
-                            style: TextStyle(
-                              color: Colors.grey
-                            ),
-                            )
-                        ],
-                      )
-                    ]
-                    ),
-                );
+            elevation: 5,
+            margin: EdgeInsets.symmetric(vertical: 8,horizontal: 5),
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 30,
+                child: Padding(
+                  padding: const EdgeInsets.all(6),
+                  child: FittedBox(
+                    child: Text('R\$${tr.value}')),
+                ),
+              ),
+              title: Text(
+                tr.title,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold
+                ),
+                
+              ),
+              subtitle: Text(
+                DateFormat('d MMM y').format(tr.date),
+              ),
+              
+            ),
+          );
 
         },
               
