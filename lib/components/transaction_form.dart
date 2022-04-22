5631d5517cba39a1d6943befac24261ffab105d4
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2022-04-22 14:36:26 -0300

    transacão com data

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 class TransactionForm extends StatefulWidget {
 
- final void Function(String, double) onSubmit;
+ final void Function(String, double, DateTime ) onSubmit;
 
  TransactionForm(this.onSubmit);
 
@@ -10,17 +11,40 @@ class TransactionForm extends StatefulWidget {
 }
 
 class _TransactionFormState extends State<TransactionForm> {
- final titleController = TextEditingController();
+ final _titleController = TextEditingController();
 
- final valueController = TextEditingController();
+ final _valueController = TextEditingController();
+ DateTime _selectedDate = DateTime.now();
 
  _submitForm(){
-   final title =  titleController.text;
-   final value = double.tryParse(valueController.text) ?? 0.0;
-   if(title.isEmpty || value <= 0){
+   final title =  _titleController.text;
+   final value = double.tryParse(_valueController.text) ?? 0.0;
+   
+   if(title.isEmpty || value <= 0 || _selectedDate == null){
      return;
    }
-    widget.onSubmit(title,value);
+    widget.onSubmit(title,value, _selectedDate);
+ }
+ _showDatePicker(){
+  showDatePicker(
+
+    context: context,
+    initialDate: DateTime.now(),
+    firstDate: DateTime(2021),
+    lastDate: DateTime.now(),
+    
+  ).then((pickedDate){
+    if(pickedDate == null){
+      return;
+
+    }
+    setState(() {
+      _selectedDate = pickedDate;
+      
+    });
+
+  });
+  
  }
 
   @override
@@ -34,7 +58,7 @@ class _TransactionFormState extends State<TransactionForm> {
                 child: Column(
                   children: [
                     TextField(
-                      controller: titleController,
+                      controller: _titleController,
                       onSubmitted: (_) =>_submitForm(),
                       decoration: InputDecoration(
                         labelText: 'Titulo'
@@ -42,13 +66,32 @@ class _TransactionFormState extends State<TransactionForm> {
                       
                     ),
                     TextField(
-                     controller: valueController,
+                     controller: _valueController,
                      keyboardType: TextInputType.number,
                      onSubmitted:(_)=> _submitForm() ,
                       decoration: InputDecoration(
                         labelText: 'Valor (R\$)'
                       ),
                       
+                    ),
+                    Container(
+                      height: 70,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              _selectedDate == null ?'Nenhuma data Selecionada' : 'Data Selecionada: ${DateFormat('dd/MM/y').format(_selectedDate)}',
+                          
+                              ),
+                          ),
+                          ElevatedButton(
+                            onPressed: _showDatePicker, 
+                            child: Text('Selecionar Data'),
+                            )
+
+                        ],
+
+                      ),
                     ),
                     Row(
                      mainAxisAlignment: MainAxisAlignment.end,
--- lib/main.dart
+++ lib/main.dart
@@ -38,28 +38,8 @@ class Despesas extends StatefulWidget {
 
 class _DespesasState extends State<Despesas> {
 
-  final List<Transaction> _transactions = [
-    Transaction(
-      id: 't0',
-      title: 'Conta antiga', 
-      value: 400.0,
-       date: DateTime.now().subtract(Duration(days:33))
-    ),
-    Transaction(
-      id: 't1',
-      title: 'tenis', 
-      value: 400.0,
-       date: DateTime.now().subtract(Duration(days:3))
-    ),
-      Transaction(
-      id: 't2',
-      title: 'Conta luz', 
-     value: 211.30,
-      date: DateTime.now().subtract(Duration(days:4))
-      ),
-
-
-  ];
+  final List<Transaction> _transactions = [];
+  
   List<Transaction> get _recentTrasanctions {
     return _transactions .where((tr){
       return tr.date.isAfter(DateTime.now().subtract(
@@ -69,13 +49,13 @@ class _DespesasState extends State<Despesas> {
     }).toList();
   }
 
-   _addTrasaction(String title, double value){
+   _addTrasaction(String title, double value, DateTime date){
 
     final newTransaction = Transaction(
       id: Random().nextDouble().toString(), 
       title: title, 
       value: value, 
-      date: DateTime.now()
+      date: date,
       );
       setState(() {
         _transactions.add(newTransaction);
@@ -89,7 +69,7 @@ _openTransactionFormModel(BuildContext context){
      builder: (_) {
        return TransactionForm(_addTrasaction);
 
-     }
+     },
      );
 }
 
