b05e8d5dcd836070907361abb5c106a27d10a114
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2022-04-21 22:17:42 -0300

    finalizando chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+
+
 import 'package:despesas/components/transaction_form.dart';
 import 'package:despesas/components/transaction_list.dart';
 
-import 'package:despesas/model/Transaction.dart';
+import './model/Transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../model/Transaction.dart';
 import 'dart:math';
+import 'components/Chart.dart';
 
 void main(){
   runApp(Home());
@@ -36,21 +39,36 @@ class Despesas extends StatefulWidget {
 class _DespesasState extends State<Despesas> {
 
   final List<Transaction> _transactions = [
-   // Transaction(
-    //  id: 't1',
-    //  title: 'Novo tenis de corrida', 
-    //  value: 310.0,
-    //   date: DateTime.now()
-    //  ),
-    //  Transaction(
-     // id: 't2',
-    //  title: 'Conta luz', 
-     // value: 211.30,
-    //   date: DateTime.now()
-    //  ),
+    Transaction(
+      id: 't0',
+      title: 'Conta antiga', 
+      value: 400.0,
+       date: DateTime.now().subtract(Duration(days:33))
+    ),
+    Transaction(
+      id: 't1',
+      title: 'tenis', 
+      value: 400.0,
+       date: DateTime.now().subtract(Duration(days:3))
+    ),
+      Transaction(
+      id: 't2',
+      title: 'Conta luz', 
+     value: 211.30,
+      date: DateTime.now().subtract(Duration(days:4))
+      ),
 
 
   ];
+  List<Transaction> get _recentTrasanctions {
+    return _transactions .where((tr){
+      return tr.date.isAfter(DateTime.now().subtract(
+        Duration(days: 7),
+      ));
+
+    }).toList();
+  }
+
    _addTrasaction(String title, double value){
 
     final newTransaction = Transaction(
@@ -95,15 +113,8 @@ _openTransactionFormModel(BuildContext context){
           crossAxisAlignment: CrossAxisAlignment.stretch,
          
           children: [
-            Container(
-            
-              child: Card(
-                color: Colors.blue,
-                child: Text("grafico"),
-                elevation: 5,
-      
-              ),
-            ),
+            Chart(_recentTrasanctions ),
+
             TransactionList(_transactions),
             
             
